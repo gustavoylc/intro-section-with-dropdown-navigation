a7bc0ba1828f6efc3f07b3696c1a48cc62c27864
Gustavo Leyte-Vidal Chacón <glcdmx@gmail.com>
2022-10-10 11:15:38 -0400

    feat: add colors and font config with tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Epilogue", ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				"footer-primary": "hsl(228, 45%, 44%)",
+				"almost-white": "hsl(0, 0%, 98%)",
+				"medium-gray": "hsl(0, 0%, 41%)",
+				"almost-black": "hsl(0, 0%, 8%)",
+			},
+		},
 	},
 	plugins: [],
 };
