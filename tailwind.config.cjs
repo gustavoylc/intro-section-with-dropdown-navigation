6607220b6a534bb95c314e2a867c853d292cfe08
Gustavo Leyte-Vidal Chacón <glcdmx@gmail.com>
2022-10-12 17:16:08 -0400

    style: add new transition duration value

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		minWidth: {
+			375: "375px",
+		},
 		extend: {
 			fontFamily: {
 				sans: ["Epilogue", ...defaultTheme.fontFamily.sans],
@@ -13,6 +16,9 @@ module.exports = {
 				"almost-white": "hsl(0, 0%, 98%)",
 				"medium-gray": "hsl(0, 0%, 41%)",
 				"almost-black": "hsl(0, 0%, 8%)",
+			},
+			transitionDuration: {
+				1500: "1500ms",
 			},
 		},
 	},
